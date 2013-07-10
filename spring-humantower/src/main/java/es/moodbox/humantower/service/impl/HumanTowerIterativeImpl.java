891114c5e61875b59d10d07abb169b28701ad781
victor <victor@Aliencito>
2013-07-11 01:03:49 +0200

    a

--- spring-humantower/src/main/java/es/moodbox/humantower/service/impl/HumanTowerIterativeImpl.java
+++ spring-humantower/src/main/java/es/moodbox/humantower/service/impl/HumanTowerIterativeImpl.java
@@ -5,6 +5,13 @@ import java.util.HashMap;
 import es.moodbox.humantower.service.HumanTowerService;
 
 
+/**
+ * Calculates the human tower weight of a desired level / index (matrix)
+ * Using iteration to fulfill the structure below the position
+ * 
+ * @author victor
+ *
+ */
 public class HumanTowerIterativeImpl implements HumanTowerService{
 
 	private final static int HUMAN_WEIGTH = 50;
@@ -20,7 +27,7 @@ public class HumanTowerIterativeImpl implements HumanTowerService{
 	}
 	
 	/**
-	 * If not tell is it at the edge
+	 * Calculates the weight of an edge position at the level
 	 * 
 	 * @param level
 	 * 
@@ -34,25 +41,23 @@ public class HumanTowerIterativeImpl implements HumanTowerService{
 			return (HUMAN_WEIGTH + getHumanEdgeWeight(level-1)) / 2;
 		}
 	}
+
 	public Double getHumanEdgeWeight(int level, int index) {
-		//i will be level
-		//j will be index
 		Double result;
 		for (int i = 1; i <= level; i++){
-			//System.out.println("At level= "+i);
 			for (int j = 0; j <= i; j++) {
-				//peso TOTAL de lo anterior es lo que soporto
-				//edge
-				if (j == 0) {
+				
+				//it's an edge
+				if (j == 0 || j == i) {
 					result = ((HUMAN_WEIGTH + getSafeHumanWeight(i - 1, 0)) / 2);
-				} else if (j == i) {
-					result = ((HUMAN_WEIGTH + getSafeHumanWeight(i - 1, 0)) / 2);
+				//intern position
 				} else {
 					result = HUMAN_WEIGTH + ((getSafeHumanWeight(i - 1, j - 1) + getSafeHumanWeight(i - 1, j)) / 2);
 				}
-				//System.out.println("���� Adding= ("+i+"," +j+") w: "+result);
+				//add to the internal structure for compute the next level
 				humanTower.put(getKey(i, j) , result);
 				
+				//if it is the desired result
 				if (i == level && j == index) {
 					return result;
 				}
