6c84a18d88b3ebc55e2a58652664639c9b651f6d
victor <victor@Aliencito>
2013-07-11 01:52:06 +0200

    Enough for today!

--- spring-humantower/src/main/java/es/moodbox/humantower/handlers/util/RequestUtil.java
+++ spring-humantower/src/main/java/es/moodbox/humantower/handlers/util/RequestUtil.java
@@ -3,14 +3,24 @@ package es.moodbox.humantower.handlers.util;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
+/**
+ * Utilities to manage the request
+ * 
+ * @author victor
+ *
+ */
 public class RequestUtil {
 
+	private static final String EQUAL = "=";
+	private static final String DELIM = "&";
+	
+	
 	public static HashMap<String, String> getParams(String paramaters) {
-		StringTokenizer paramGroup = new StringTokenizer(paramaters, "&");
+		StringTokenizer paramGroup = new StringTokenizer(paramaters, DELIM);
 
 		HashMap<String, String> params = new HashMap<String, String>();
 		while(paramGroup.hasMoreTokens()){
-			StringTokenizer value = new StringTokenizer(paramGroup.nextToken(), "=");
+			StringTokenizer value = new StringTokenizer(paramGroup.nextToken(), EQUAL);
 			params.put(value.nextToken(), value.nextToken());
 		}
 		return params;
